98c1e8d4873f636cd717da8193156f84de350de1
SamuelUrdaneta2.0 <samuel_urdaneta@>
2026-07-02 08:36:04 -0300

    add- conclusão do exercico 2

--- ex_02.php
+++ ex_02.php
@@ -1,6 +1,15 @@
 <?php
 
+function inverterTexto($text)
+{
 
+    $invertido = strrev($text);
+    $contaLetras = mb_strlen($text);
 
+    echo "A palavra é: $text <br>";
+    echo "A palavra ao contrario é: $invertido <br>";
+    echo "A quantidade de letras são: $contaLetras <br>";
+}
+$text = "Danilo Rachadel Jr";
 
-?>
+inverterTexto($text);

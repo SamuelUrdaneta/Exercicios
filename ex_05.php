e88ec3d8ceeab89aa8d43d4c0b8b9eccc46ba12c
SamuelUrdaneta2.0 <samuel_urdaneta@>
2026-07-07 08:31:47 -0300

    add- terminei o exercicio 5

---
+++ ex_05.php
@@ -0,0 +1,13 @@
+<?php
+
+function analisarTexto($texto){
+
+    $textom = strtolower($texto)
+    $quantletras = mb_strlen($textom);
+    $quantpalavras = str_word_count($textom);
+    
+
+}
+
+
+?>
